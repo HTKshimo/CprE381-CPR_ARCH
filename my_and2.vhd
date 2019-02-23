9b69a8ebd15b4c05a66f62e9e5dcbd4722f822db
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:46:47 -0600

    Update my_and2.vhd

--- my_and2.vhd
+++ my_and2.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity my_and2 is
+entity and2 is
 
   port(i_A          : in std_logic;
        i_B          : in std_logic;
        o_F          : out std_logic);
 
-end my_and2;
+end and2;
 
-architecture dataflow of my_and2 is
+architecture dataflow of and2 is
 begin
 
   o_F <= i_A AND i_B;
