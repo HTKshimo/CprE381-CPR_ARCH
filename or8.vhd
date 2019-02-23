0b277e7097d77df957adc49337559a8d94247f0b
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:48:20 -0600

    Update and rename my_or8.vhd to or8.vhd

--- or8.vhd
+++ or8.vhd
@@ -1,12 +1,12 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity my_or8 is
+entity or8 is
   port(i0, i1, i2, i3, i4, i5, i6, i7 : in std_logic;
        output : out std_logic);
-end my_or8;
+end or8;
 
-architecture behavior of my_or8 is
+architecture behavior of or8 is
 begin
   process
   begin
