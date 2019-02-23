bcff3a23807b828d9a80a092ae9203fa66878288
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:36:23 -0600

    Update mux2to1.vhd

--- mux2to1.vhd
+++ mux2to1.vhd
@@ -10,18 +10,18 @@ entity mux2to1 is
 end mux2to1;
 
 architecture structure of mux2to1 is
-component my_inv
+component inv
   port(input          : in std_logic;
        output          : out std_logic);
 end component;
 
-component my_or2
+component or2
   port(i_A        : in std_logic;
        i_B        : in std_logic;
        o_F         : out std_logic);
 end component;
 
-component my_and2
+component and2
   port(i_A       : in std_logic;
        i_B       : in std_logic;
        o_F        : out std_logic);
@@ -30,13 +30,13 @@ end component;
 signal s1, s2, s3   : std_logic;
 
 begin
-  inversion_1: my_inv 
+  inversion_1: inv 
     port map(i_X, s1);
-  and2_1: my_and2
+  and2_1: and2
     port map(i_A, s1, s2);
-  and2_2: my_and2
+  and2_2: and2
     port map(i_X, i_B, s3);
-  or_1: my_or2
+  or_1: or2
     port map(s2, s3, o_Y);
 
 end structure;
