7a60b524462bd65487411b3a7c50cd461a47de7d
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:47:30 -0600

    Update my_or2.vhd

--- my_or2.vhd
+++ my_or2.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity my_or2 is
+entity or2 is
 
   port(i_A          : in std_logic;
        i_B          : in std_logic;
        o_F          : out std_logic);
 
-end my_or2;
+end or2;
 
-architecture dataflow of my_or2 is
+architecture dataflow of or2 is
 begin
 
   o_F <= i_A or i_B;
