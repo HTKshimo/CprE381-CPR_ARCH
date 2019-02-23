0901c505d48763ca3a7eea39dafec34a35e2b8b1
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:41:10 -0600

    Update mux2to1Nbit.vhd

--- mux2to1Nbit.vhd
+++ mux2to1Nbit.vhd
@@ -11,18 +11,18 @@ entity mux2to1Nbit is
 end mux2to1Nbit;
 
 architecture structure of mux2to1Nbit is
-component my_inv
+component inv
   port(input          : in std_logic;
        output          : out std_logic);
 end component;
 
-component my_or2
+component or2
   port(i_A          : in std_logic;
        i_B          : in std_logic;
        o_F          : out std_logic);
 end component;
 
-component my_and2
+component and2
   port(i_A          : in std_logic;
        i_B          : in std_logic;
        o_F          : out std_logic);
@@ -32,13 +32,13 @@ signal s1, s2, s3   : std_logic_vector(N-1 downto 0);
 
 begin
 G1: for i in 0 to N-1 generate
-  inversion_i: my_inv 
+  inversion_i: inv 
     port map(i_X, s1(i));
-  and1_i: my_and2
+  and1_i: and2
     port map(i_A(i), s1(i), s2(i));
-  and2_i: my_and2
+  and2_i: and2
     port map(i_X, i_B(i), s3(i));
-  or2_i: my_or2
+  or2_i: or2
     port map(s2(i), s3(i), o_Y(i));
 end generate;
 
