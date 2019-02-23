e85b00cbf0d43caa47725991e41ce54b58ca1c05
AnhdTo <47896639+AnhdTo@users.noreply.github.com>
2019-02-22 20:49:12 -0600

    Update and rename my_xor2.vhd to xor2.vhd

--- xor2.vhd
+++ xor2.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity my_xor2 is
+entity xor2 is
 
   port(i_A          : in std_logic;
        i_B          : in std_logic;
        o_F          : out std_logic);
 
-end my_xor2;
+end xor2;
 
 architecture dataflow of my_xor2 is
 begin
